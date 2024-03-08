b82045a72fdf61c842c1530e0211188012dbf665
Rasha <rasharaiyan00@gmail.com>
2024-03-08 20:10:57 +0200

    test get_coin_by_id

---
+++ logic/GetCoinById.py
@@ -0,0 +1,8 @@
+class GetCoinByID:
+
+    def __init__(self, api_wrapper):
+        self.api_wrapper = api_wrapper
+
+    def get_coin_by_id(self, coin_id):
+        endpoint = f"/v1/coins/{coin_id}"
+        return self.api_wrapper.api_get_request(endpoint)
---
+++ tests/test_get_coin_by_id.py
@@ -0,0 +1,26 @@
+import unittest
+from infra.api_wrapper import APIWrapper
+from logic.GetCoinById import GetCoinByID
+from logic.ListCoins import ListCoins
+from tests.test_utils import TestBase
+
+
+class TestGetCoinByID(TestBase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.service = GetCoinByID(
+            APIWrapper("https://api.coinpaprika.com", None))
+
+
+    def test_get_coin_by_id(self):
+        coin_id = 'btc-bitcoin'
+        response = self.service.get_coin_by_id(coin_id)
+        self.assertEqual(response['id'], coin_id)
+        self.assertIsInstance(response['name'], str)
+        self.assertIsInstance(response['symbol'], str)
+        self.assertIsInstance(response['rank'], int)
+        self.assertIsInstance(response['is_new'], bool)
+        self.assertIsInstance(response['is_active'], bool)
+        self.assertIn(response['type'], ['coin', 'token'])
+        # Additional checks based on response schema
